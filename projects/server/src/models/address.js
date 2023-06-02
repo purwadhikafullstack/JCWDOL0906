3e0b4f4bc4c1d8e84cc6e599011f8dd94e6cc0c5
meitarizki <meitanurmala@gmail.com>
2023-06-02 11:08:27 +0700

    Profile Models

---
+++ projects/server/src/models/address.js
@@ -0,0 +1,38 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Address extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Address.belongsTo(models.User, {
+        foreignKey: {
+          name: "user_id",
+        },
+      });
+    }
+  }
+  Address.init({
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    province: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Address;
+};
