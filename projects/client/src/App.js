64e65336426a09dd9ef45a55610a6c34a196b6ff
meitarizki <meitanurmala@gmail.com>
2023-06-17 10:48:04 +0700

    update

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,30 +1,31 @@
-import axios from "axios";
-import logo from "./logo.svg";
+// import axios from "axios";
+// import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { useEffect, useState } from "react";
+import { HashRouter, BrowserRouter, Route, Routes } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from './theme/theme.js'
+import theme from "./theme/theme.js";
 import Dashboard from "./adminLayouts/Admin";
 
 function App() {
-  const [message, setMessage] = useState("");
+  // const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/greetings`
-      );
-      setMessage(data?.message || "");
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { data } = await axios.get(
+  //       `${process.env.REACT_APP_API_BASE_URL}/greetings`
+  //     );
+  //     setMessage(data?.message || "");
+  //   })();
+  // }, []);
   return (
     <ChakraProvider theme={theme} resetCss={false} position="relative">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="*" element={<Dashboard />} />
+          {/* <Route path="/admin/Products/" element={<Dashboard />} /> */}
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ChakraProvider>
   );
 }
