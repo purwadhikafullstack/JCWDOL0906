f5628bed4705d1d79e8038c149a67ff8920a18e2
meitarizki <meitanurmala@gmail.com>
2023-06-17 10:51:06 +0700

    update

---
+++ projects/server/src/controllers/index.js
@@ -0,0 +1,7 @@
+const authControllers = require("./authControllers");
+const productControllers = require("./productControllers");
+
+module.exports = {
+  authControllers,
+  productControllers,
+};
