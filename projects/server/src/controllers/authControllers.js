6f35da4d08443c89b792db0af88820f8d04df87f
meitarizki <meitanurmala@gmail.com>
2023-06-17 10:50:54 +0700

    auth

--- projects/server/src/controllers/authControllers.js
+++ projects/server/src/controllers/authControllers.js
@@ -1,5 +1,5 @@
 // import sequelize
-const Sequelize  = require("sequelize");
+const Sequelize = require("sequelize");
 // import model
 const db = require("../models");
 const user = db.User;
@@ -13,97 +13,105 @@ const transporter = require("../helpers/transporter");
 const fs = require("fs");
 const handlebars = require("handlebars");
 module.exports = {
-    register: async (req, res) => {
-        try {
-            const result = await db.sequelize.transaction(async (t) => { 
-            const { username, email, phone_number, password, password_confirmation } =
-                req.body;
+  register: async (req, res) => {
+    try {
+      const result = await db.sequelize.transaction(async (t) => {
+        const {
+          username,
+          email,
+          phone_number,
+          password,
+          password_confirmation,
+        } = req.body;
 
-            console.log(req.body);
+        console.log(req.body);
 
-            if (!username || !email || !phone_number || !password)
-                throw "Please complete your data";
-            
-            if (isNaN(phone_number)) 
-            throw "Please input a number";
+        if (!username || !email || !phone_number || !password)
+          throw "Please complete your data";
 
-            if (phone_number.length < 8 || phone_number.length > 13)
-            throw "Please input your valid phone number"
+        if (isNaN(phone_number)) throw "Please input a number";
 
-            if (password !== password_confirmation)
-                throw "Password does not match";
+        if (phone_number.length < 8 || phone_number.length > 13)
+          throw "Please input your valid phone number";
 
-            const passwordRegex =
-                /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[0-9a-zA-Z!@#$%^&*()_+]{8,}$/;
-            if (!passwordRegex.test(password))
-                throw "Password must contain at least 8 characters including an uppercase letter, a symbol, and a number";
+        if (password !== password_confirmation) throw "Password does not match";
 
-            const salt = await bcrypt.genSalt(10);
-            const hashPass = await bcrypt.hash(password, salt);
+        const passwordRegex =
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[0-9a-zA-Z!@#$%^&*()_+]{8,}$/;
+        if (!passwordRegex.test(password))
+          throw "Password must contain at least 8 characters including an uppercase letter, a symbol, and a number";
 
-            const generateVerticationToken = (username) => {
-                let token = jwt.sign({ username }, "g-medsnial", {
-                    expiresIn: "9999 years",
-                });
-                return token;
-            };
+        const salt = await bcrypt.genSalt(10);
+        const hashPass = await bcrypt.hash(password, salt);
 
-            const result = await user.create({
-                username,
-                email,
-                phone_number,
-                password: hashPass,
-            },{t});
+        const generateVerticationToken = (username) => {
+          let token = jwt.sign({ username }, "g-medsnial", {
+            expiresIn: "9999 years",
+          });
+          return token;
+        };
 
-            let payload = { id: result.id };
-            let token = jwt.sign(payload, "g-medsnial", {
-                expiresIn: "9999 years",
-            });
+        const result = await user.create(
+          {
+            username,
+            email,
+            phone_number,
+            password: hashPass,
+          },
+          { t }
+        );
 
-            await user.update(
-                { verification_token: token },
-                {
-                    where: {
-                        id: result.id,
-                    },
-                }, {t}
-            );
-            
-
-            const verificationLink = `http://localhost:3000/verification/${token}`;
-
-            const tempEmail = fs.readFileSync(require.resolve("../templates/confirmation.html"),{ encoding: "utf8"});
-            // console.log (tempEmail);
-            const tempCompile = handlebars.compile(tempEmail);
-            const tempResult = tempCompile({ username, verificationLink });
-      
-            await transporter.sendMail(
-              {
-                from: `G-Medsnial <gmedsnial@gmial.com}>`,
-                to: email,
-                subject: "Verify Your Account",
-                html: tempResult,
-                
-              },
-              (error, info) => {
-                if (error) {
-                throw new Error();
-                  console.log(error);
-                } else {
-                  console.log("Email sent: " + info.response);
-                }
-              }
-            );
-            return result;
+        let payload = { id: result.id };
+        let token = jwt.sign(payload, "g-medsnial", {
+          expiresIn: "9999 years",
         });
-        res.status(200).send({
-            status: true,
-            data: result,
-            message: "register success",
-        });
-        } catch (err) {
-            console.log(err);
-            res.status(400).send(err);
-        }
+
+        await user.update(
+          { verification_token: token },
+          {
+            where: {
+              id: result.id,
+            },
+          },
+          { t }
+        );
+
+        const verificationLink = `http://localhost:3000/verification/${token}`;
+
+        const tempEmail = fs.readFileSync(
+          require.resolve("../templates/confirmation.html"),
+          { encoding: "utf8" }
+        );
+        // console.log (tempEmail);
+        const tempCompile = handlebars.compile(tempEmail);
+        const tempResult = tempCompile({ username, verificationLink });
+
+        await transporter.sendMail(
+          {
+            from: `G-Medsnial <gmedsnial@gmial.com}>`,
+            to: email,
+            subject: "Verify Your Account",
+            html: tempResult,
+          },
+          (error, info) => {
+            if (error) {
+              throw new Error();
+              console.log(error);
+            } else {
+              console.log("Email sent: " + info.response);
+            }
+          }
+        );
+        return result;
+      });
+      res.status(200).send({
+        status: true,
+        data: result,
+        message: "register success",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).send(err);
     }
-}
+  },
+};
