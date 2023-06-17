8ca7eddace1a28d69428d3247f5dc7291164d85b
meitarizki <meitanurmala@gmail.com>
2023-06-17 10:51:57 +0700

    product models

--- projects/server/src/models/product.js
+++ projects/server/src/models/product.js
@@ -1,79 +1,77 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    class Product extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-            // Product.hasOne(models.Product_Details, {
-            //     foreignKey: {
-            //         name: "product_id",
-            //     },
-            // });
-            // Product.hasMany(models.Category, {
-            //     foreignKey: {
-            //         name: "category_id",
-            //     }
-            // });
-            // Product.hasMany(models.Discount, {
-            //     foreignKey: {
-            //         name: "product_id",
-            //     }
-            // });
-            // Product.hasMany(models.Transaction_Detail, {
-            //     foreignKey: {
-            //         name: "product_id",
-            //     }
-            // });
-            // Product.hasMany(models.Stock, {
-            //     foreignKey: {
-            //         name: "product_id"
-            //     }
-            // });
-            
-        }
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      // Product.hasOne(models.Product_Details, {
+      //     foreignKey: {
+      //         name: "product_id",
+      //     },
+      // });
+      // Product.hasMany(models.Category, {
+      //     foreignKey: {
+      //         name: "category_id",
+      //     }
+      // });
+      // Product.hasMany(models.Discount, {
+      //     foreignKey: {
+      //         name: "product_id",
+      //     }
+      // });
+      // Product.hasMany(models.Transaction_Detail, {
+      //     foreignKey: {
+      //         name: "product_id",
+      //     }
+      // });
+      // Product.hasMany(models.Stock, {
+      //     foreignKey: {
+      //         name: "product_id"
+      //     }
+      // });
     }
-    Product.init(
-        {
-            product_name: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                unique: "product_name",
-            },
-            price: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                defaultValue: false,
-            },
-            image: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                unique: "image"
-            },
-            is_deleted: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                defaultValue: false,
-            },
-            createdBy: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                defaultValue: false,
-            },
-            updatedBy: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                defaultValue: false,
-            },
-        },
-        {
-            sequelize,
-            modelName: "Product",
-        }
-    );
-    return Product;
+  }
+  Product.init(
+    {
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: "product_name",
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      is_deleted: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: false,
+      },
+      createdBy: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: false,
+      },
+      updatedBy: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Product",
+    }
+  );
+  return Product;
 };
