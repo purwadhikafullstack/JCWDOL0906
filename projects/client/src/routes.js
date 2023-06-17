ef94c716c5fb646ec014af95c1921605040e9713
meitarizki <meitanurmala@gmail.com>
2023-06-17 10:48:18 +0700

    update routes

--- projects/client/src/routes.js
+++ projects/client/src/routes.js
@@ -1,5 +1,5 @@
 // import
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Dashboard from "views/Dashboard/Dashboard.js";
 // import Tables from "views/Dashboard/Tables.js";
 // import Billing from "views/Dashboard/Billing.js";
@@ -12,34 +12,32 @@ import {
   HomeIcon,
   StatsIcon,
   CreditIcon,
-  PersonIcon,
-  DocumentIcon,
-  RocketIcon,
   SupportIcon,
 } from "./adminComponents/Icons/Icons";
+import Product from "./adminComponents/Products";
 
 var dashRoutes = [
   {
     path: "/dashboard",
     name: "Dashboard",
     rtlName: "لوحة القيادة",
-    icon: <HomeIcon color='inherit' />,
+    icon: <HomeIcon color="inherit" />,
     // component: Dashboard,
     layout: "/admin",
   },
   {
-    path: "/tables",
-    name: "Product",
+    path: "/products",
+    name: "Products",
     rtlName: "لوحة القيادة",
-    icon: <StatsIcon color='inherit' />,
-    // component: Tables,
+    icon: <StatsIcon color="inherit" />,
+    component: Product,
     layout: "/admin",
   },
   {
     path: "/billing",
     name: "Category",
     rtlName: "لوحة القيادة",
-    icon: <CreditIcon color='inherit' />,
+    icon: <CreditIcon color="inherit" />,
     // component: Billing,
     layout: "/admin",
   },
@@ -47,7 +45,7 @@ var dashRoutes = [
     path: "/rtl-support-page",
     name: "Report",
     rtlName: "آرتيإل",
-    icon: <SupportIcon color='inherit' />,
+    icon: <SupportIcon color="inherit" />,
     // component: RTLPage,
     layout: "/rtl",
   },
@@ -55,7 +53,7 @@ var dashRoutes = [
     path: "/rtl-support-page",
     name: "Q&A",
     rtlName: "آرتيإل",
-    icon: <SupportIcon color='inherit' />,
+    icon: <SupportIcon color="inherit" />,
     // component: RTLPage,
     layout: "/rtl",
   },
