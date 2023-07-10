48fd289ad18b4133e533939e185aaac0ae74f659
michelangel20 <mluntungan194@gmail.com>
2023-07-10 19:44:48 +0700

    Update

--- projects/server/src/router/authRouter.js
+++ projects/server/src/router/authRouter.js
@@ -6,7 +6,7 @@ router.post("/auth/register", c_auth.register);
 router.post("/auth/login", c_auth.login);
 router.post("/auth/verification", login, c_auth.verification);
 router.post("/auth/reset-password", login, c_auth.reset_password);
-router.post("/auth/confir-memail", c_auth.confirm_email);
+router.post("/auth/confirm-email", c_auth.confirm_email);
 router.get("/auth/keep-login", login, c_auth.keep_login);
 
 
